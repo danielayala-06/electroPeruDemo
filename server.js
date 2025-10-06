29615e7b8c78201538fc5f41ada6f7d230834586
Daniel jesús <danielayalaromo8@gmail.com>
2025-10-06 09:49:17 -0500

    Front-End Terminado

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express')
+
+//Actualización para desplegar el Front-End
+const cors = require('cors')//Permisos sobre el contenido a deplegar
+const path = require('path') //Express servir el Front-End
+
+
 const productoRoutes = require('./routes/productoRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 3000 //Puerto de la aplicación
+
+//Acualización - Permisos Cors
+app.use(cors({
+    origin: '*',
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
+    credentials: true
+}))
+//Actualización: Vamos a servir los documentos HTML, CSS, JS
+app.use(express.static(path.join(__dirname, 'public')))
+
+//http://localhost:3000 ->public>index.html
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+}) 
 
 //Comunicación se realizará JSON
 app.use(express.json())
