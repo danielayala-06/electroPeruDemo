abe4e589ba71bf02967cae795fce33de4c913edb
Daniel Ayala <danielayala8@gmail.com>
2025-10-07 18:36:01 -0500

    Se agrego y configuro el archivo para las rutas:get y post. Ya funcionan los metodos para insertar y listar

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require('express')
 const cors = require('cors')//Permisos sobre el contenido a deplegar
 const path = require('path') //Express servir el Front-End
 
-
+// 
 const productoRoutes = require('./routes/productoRoutes')
+const clientesRoutes = require('./routes/clienteRoutes')
 
 const app = express()
 const PORT = process.env.PORT || 3000 //Puerto de la aplicación
@@ -29,6 +30,7 @@ app.use(express.json())
 
 //Rutas
 app.use('/api/productos', productoRoutes)
+app.use('/api/clientes', clientesRoutes)
 
 //Iniciar el servidor
 app.listen(PORT, ()=>{
